5ad3e28e1b5b783b930e6be529181b1fe3a27746
Miguelgsd <miguelcbdutra@gmail.com>
2026-05-02 14:02:45 -0300

    criação dos métodos iniciais da classe GerenciadorTurma; para testes

---
+++ GerenciadorTurma.java
@@ -0,0 +1,52 @@
+public class GerenciadorTurma {
+    private String nomeTurma;
+    private Aluno[] arrayAluno;
+    private int i;
+
+    public GerenciadorTurma(String nomeTurma){
+        this.nomeTurma = nomeTurma;
+        this.arrayAluno = Aluno[4];
+        this.i = 0;
+    }
+
+    public void cadastrarAluno(Aluno a){
+        if(i < arrayAluno.length){
+            arrayAluno[i] = a;
+            i += 1;
+        } else {
+            System.out.println("Turma com capacidade máxima atingida.");
+        }
+    }
+
+    public void emitirBoletins(){
+        System.out.println("Emissão dos Boletins: \n---------------------------");
+        for(int j = 0; j < i; j++){
+            return arrayAluno[j].exibirBoletim();
+        }
+    }
+
+    public int contarAprovados(){
+        for(int j = 0; j < i; j++){
+            int aprovados = 0;
+
+            if(arrayAluno[j].calcularMedia() >= 6.0){
+                aprovados += 1;
+            }
+
+            return aprovados;
+        }
+    }
+
+    public void exibirMaiorMedia(){
+        double maiorMedia = 0;
+        String maiorAluno = "";
+        for(int j = 0; j < i; j++){
+            if(maiorMedia < arrayAluno[i].calcularMedia()){
+                maiorMedia = arrayAluno[i].calcularMedia();
+                maiorAluno = arrayAluno[i].getAluno();
+            }
+        }
+
+        System.out.println("O aluno com a maior média foi: " + maiorAluno + "\nSua nota foi: " + maiorMedia);
+    }
+}
