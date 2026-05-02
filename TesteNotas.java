7ec9692a0ae7084f3803a6b0ca61813caa5b6f92
Miguelgsd <miguelcbdutra@gmail.com>
2026-05-02 15:30:49 -0300

    Implementação do looping

--- TesteNotas.java
+++ TesteNotas.java
@@ -15,52 +15,55 @@ public class TesteNotas{
         turma1.cadastrarAluno(aluno3);
 
         System.out.println("\n===== SISTEMA DE NOTAS - 3° Ano A =====");
-        System.out.println("1 - Emitir boletins de todos os alunos.\n2 - Exibir quantidade de alunos aprovados.\n3 - Exibir aluno com maior média\n4 - Lançar notas para um aluno pelo índice\n0 - Sair do sistema");
-        System.out.print("\nEscolha uma opção: ");
-        opcao = input.nextInt();
-
+        
         boolean exec = true;
-        switch(opcao){
-            case 1:
-                turma1.emitirBoletins();
-                break;
+        do {
+            System.out.println("\n\n1 - Emitir boletins de todos os alunos.\n2 - Exibir quantidade de alunos aprovados.\n3 - Exibir aluno com maior média\n4 - Lançar notas para um aluno pelo índice\n0 - Sair do sistema");
+            System.out.print("\nEscolha uma opção: ");
+            opcao = input.nextInt();
+            switch(opcao){
+                case 1:
+                    turma1.emitirBoletins();
+                    break;
 
-            case 2:
-                turma1.contarAprovados();
-                break;
+                case 2:
+                    turma1.contarAprovados();
+                    break;
 
-            case 3:
-                turma1.exibirMaiorMedia();
-                break;
+                case 3:
+                    turma1.exibirMaiorMedia();
+                    break;
 
-            case 4:
-                int indice;
-                double n1, n2, n3;
-                System.out.println("Digite o índice do aluno (0, 1, 2): ");
-                indice = input.nextInt();
-            
-                System.out.println("Digite as 3 notas bimestrais (pressione Enter após cada nota):");
-                n1 = input.nextDouble();
-                n2 = input.nextDouble();
-                n3 = input.nextDouble();
+                case 4:
+                    int indice;
+                    double n1, n2, n3;
+                    System.out.println("Digite o índice do aluno (0, 1, 2): ");
+                    indice = input.nextInt();
+                
+                    System.out.println("Digite as 3 notas bimestrais (pressione Enter após cada nota):");
+                    n1 = input.nextDouble();
+                    n2 = input.nextDouble();
+                    n3 = input.nextDouble();
 
-                if(indice == 0){
-                    aluno1.lancarNotas(n1, n2, n3);
-                } else if(indice == 1){
-                    aluno2.lancarNotas(n1, n2, n3);
-                } else if(indice == 2){
-                    aluno3.lancarNotas(n1, n2, n3);
-                } else {System.out.println("Opção de índice inválida.");}
-                break;
+                    if(indice == 0){
+                        aluno1.lancarNotas(n1, n2, n3);
+                    } else if(indice == 1){
+                        aluno2.lancarNotas(n1, n2, n3);
+                    } else if(indice == 2){
+                        aluno3.lancarNotas(n1, n2, n3);
+                    } else {System.out.println("Opção de índice inválida.");}
+                    break;
 
-            case 0:
-                exec = false;
-                System.out.println("Sistema encerrado.");
-                break;
+                case 0:
+                    exec = false;
+                    System.out.println("Sistema encerrado.");
+                    break;
 
-            default:
-                System.out.println("Opção inválida. Tente novamente.");
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+                    break;
 
-        }
+            }
+        } while(exec);
     }
 }
